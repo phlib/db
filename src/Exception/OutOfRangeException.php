ac20e7e95f3ee8a10b30c52f545d3c7516b927a3
qubit05 <luke.richards+github@qubit.co.uk>
2015-10-27 10:46:09 +0000

    Created out of range exception.
    Added out of range exception.

---
+++ src/Exception/OutOfRangeException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Phlib\Db\Exception;
+
+class OutOfRangeException extends \OutOfRangeException implements Exception
+{
+}
--- src/Helper/HelperInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Phlib\Db\Helper;
-
-interface HelperInterface
-{
-}
