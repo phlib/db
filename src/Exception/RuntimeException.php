d9c396270a01c67689043b155e460171e550a51e
qubit05 <luke.richards+github@qubit.co.uk>
2016-03-23 12:46:59 +0000

    Add method for creating a DB exception from a normal exception

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -4,7 +4,20 @@ namespace Phlib\Db\Exception;
 
 class RuntimeException extends \PDOException implements Exception
 {
-    public static function hasServerGoneAway($exception)
+    /**
+     * @param \Exception $exception
+     * @return static
+     */
+    public static function create(\Exception $exception)
+    {
+        return new static($exception->getMessage(), $exception->getCode(), $exception);
+    }
+
+    /**
+     * @param \PDOException $exception
+     * @return bool
+     */
+    public static function hasServerGoneAway(\PDOException $exception)
     {
         return stripos($exception->getMessage(), 'MySQL server has gone away') !== false;
     }
